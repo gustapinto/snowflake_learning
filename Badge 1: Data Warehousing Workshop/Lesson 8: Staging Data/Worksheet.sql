779ad826957bc110497cd842a69c03f1280b2e4e
Gustavo Henrique Pinto <gustavo.henrique@nappsolutions.com>
2024-03-30 20:55:53 -0300

    fix: add initial commit missing data

--- Badge 1: Data Warehousing Workshop/Lesson 8: Staging Data/Worksheet.sql
+++ Badge 1: Data Warehousing Workshop/Lesson 8: Staging Data/Worksheet.sql
@@ -1,1 +1,15 @@
 list @LIKE_A_WINDOW_INTO_AN_S3_BUCKET;
+
+list @LIKE_A_WINDOW_INTO_AN_S3_BUCKET/this_;
+
+list @LIKE_A_WINDOW_INTO_AN_S3_BUCKET/THIS_;
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW10' as step
+  ,( select count(*)
+    from UTIL_DB.INFORMATION_SCHEMA.stages
+    where stage_url='s3://uni-lab-files'
+    and stage_type='External Named') as actual
+  , 1 as expected
+  , 'External stage created' as description
+ );
